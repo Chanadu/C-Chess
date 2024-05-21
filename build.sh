ad4a641d08c36a684e9e61e800ec04218a70f6ea
Chanadu <chandu.peddada@gmail.com>
2024-05-21 18:32:03 -0500

    Build script fix

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S./ -B./build -G "Unix Makefiles"
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -S./ -B./build -G "Unix Makefiles"
 
-mkdir build
-cd build
-make
-./C-Chess
-cd ..
+make build
+./build/C-Chess
