28226a2668ab681c77e2c8ff8af280f3f4d6db4c
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-05-27 20:55:16 -0500

    improve printing

--- lib/print_board.c
+++ lib/print_board.c
@@ -1,14 +1,14 @@
 #include "print_board.h"
 
 void print_board(char board[ROWS][COLS]) {
-    printf("\n  ---------------------\n");
+    printf("\n  ┌─────────────────┐\n");
     for (int i = 0; i < ROWS; i++) {
-        printf("%d |  ", 8 - i);
+        printf("%d │ ", 8 - i);
         for (int j = 0; j < COLS; j++) {
             char output = board[i][j] == ' ' ? '~' : board[i][j];
             printf("%c ", output);
         }
-        printf(" |\n");
+        printf("│\n");
     }
-    printf("  ---------------------\n     a b c d e f g h\n\n");
+    printf("  └─────────────────┘\n     a b c d e f g h\n\n");
 }
